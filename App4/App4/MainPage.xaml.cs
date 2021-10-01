a01ed7be497ae1caebb57effe66b2ee8f604d82b
James Montemagno <james.montemagno@gmail.com>
2021-10-01 08:21:48 -0700

    Add project files.

---
+++ App4/App4/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App4
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<string> MyList { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            MyList = new List<string> { "Code 1", "Code 2" };
+            BindingContext = this;
+        }
+    }
+}
